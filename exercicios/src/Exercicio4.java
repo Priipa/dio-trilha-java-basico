ab7e887798a5359a51e9e26893b1c646e5ef3a8f
priipa <pripa.diflen@gmail.com>
2025-06-23 23:28:27 -0300

    exercicio 4 concluido

--- exercicios/src/Exercicio4.java
+++ exercicios/src/Exercicio4.java
@@ -4,6 +4,21 @@ public class Exercicio4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe um número");
+        var number = sc.nextInt();
+        var keepVerify = true;
+
+        while (keepVerify) {
+            System.out.println("Informe o número para verificação");
+            var toVerify = sc.nextInt();
+            if (toVerify < number) {
+                System.out.printf("Informe um número maior que %s\n", number);
+                continue;
+            }
+            var result = toVerify % number;     
+            keepVerify = toVerify % number == 0;
+            System.out.printf("%s %% %s = %s \n", toVerify, number, result);
+
+        }
     }
     
 }
