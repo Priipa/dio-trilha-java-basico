617e4339505053acd996bcaad5ea3a3e70387a9e
priipa <pripa.diflen@gmail.com>
2025-06-23 23:59:39 -0300

    ajustado o texto

--- exercicios/src/Exercicio1.java
+++ exercicios/src/Exercicio1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Exercicio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um número");
+        System.out.print("Digite um número: ");
         var number = sc.nextInt();
 
         for (var i = 1; i<=10; i++ ){
