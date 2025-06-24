ae680b319ed56f1bccd9bc34bf8d8aaa1de5a631
priipa <pripa.diflen@gmail.com>
2025-06-23 23:14:24 -0300

    criado o arquivo do exercicio4

---
+++ exercicios/src/Exercicio4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe um número");
+    }
+    
+}
